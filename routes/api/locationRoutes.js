02ead69b8c4adde032eb6d9cb681beeceb69efef
B Brucato <bebrucato@gmail.com>
2021-03-22 11:09:38 -0700

    routes

--- routes/api/locationRoutes.js
+++ routes/api/locationRoutes.js
@@ -1,2 +1,15 @@
 const router = require('express').Router();
 const { Location, Traveler, Trips } = require('../../models');
+
+router.get('/api/locations', async (req, res) => {
+    try {
+      const travellerData = await Location.findAll({
+        include: [{ model: Traveler }, { model: Trips }],
+      });
+      res.status(200).json(readerData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+  
